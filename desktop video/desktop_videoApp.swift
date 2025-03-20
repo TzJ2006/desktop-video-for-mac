bfc1192db00efed04c135ff4dcbad70c50077c2f
Zijia Tang <zt81@duke.edu>
2025-03-20 12:29:12 -0400

    Initial Commit

---
+++ desktop video/desktop_videoApp.swift
@@ -0,0 +1,17 @@
+//
+//  desktop_videoApp.swift
+//  desktop video
+//
+//  Created by 汤子嘉 on 3/20/25.
+//
+
+import SwiftUI
+
+@main
+struct desktop_videoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
