b465b3141d186aa1450b419eaac404890f3e2dc2
Zijia Tang <zt81@duke.edu>
2025-05-05 11:45:55 -0400

    update auto login

--- Desktop-Video/Desktop-Video/Desktop_VideoApp.swift
+++ Desktop-Video/Desktop-Video/Desktop_VideoApp.swift
@@ -6,20 +6,43 @@
 //
 
 import SwiftUI
+import ServiceManagement
 
 @main
 struct desktop_videoApp: App {
     @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     @AppStorage("useMemoryCache") var useMemoryCache: Bool = true  // Global setting
     @AppStorage("autoSyncNewScreens") var autoSyncNewScreens: Bool = true  // Added to fix missing declaration
+
+    // 新增：记录开机启动状态
+    @State private var launchAtLogin: Bool = SMAppService.mainApp.status == .enabled
+
     var body: some Scene {
         Settings {
+            VStack(alignment: .leading, spacing: 12) {
+                Toggle("开启视频缓存", isOn: $useMemoryCache)
+                Toggle("自动同步新插入的显示器", isOn: $autoSyncNewScreens)
+                Toggle("开机自启动", isOn: $launchAtLogin)
+                    .onChange(of: launchAtLogin) { newValue in
+                        do {
+                            if newValue {
+                                try SMAppService.mainApp.register()
+                            } else {
+                                try SMAppService.mainApp.unregister()
+                            }
+                        } catch {
+                            print("设置开机启动失败：\(error.localizedDescription)")
+                        }
+                    }
+            }
+            .padding()
+            .frame(width: 320)
         }
         .commands {
             CommandGroup(replacing: .appSettings) {
                 Toggle("开启视频缓存", isOn: $useMemoryCache)
                 Toggle("自动同步新插入的显示器", isOn: $autoSyncNewScreens)
-                    .padding(.bottom)
             }
             CommandGroup(replacing: .appInfo) {
                 Button("About Desktop Video") {
@@ -31,8 +54,9 @@ struct desktop_videoApp: App {
                     alert.informativeText = """
                     Desktop Video Wallpaper
                     Version \(version) (\(build))
-                    
+
                     Presented by TzJ
+                    Created just for you
                     """
                     alert.runModal()
                 }
