23171e6d27f03fb5f09ce843b95efc45a4e707f9
Zijia Tang <zt81@duke.edu>
2025-09-02 13:30:55 -0400

    feat: localize main controller

--- Desktop Vdieo/Desktop Vdieo/UI/Sidebar/SidebarView.swift
+++ Desktop Vdieo/Desktop Vdieo/UI/Sidebar/SidebarView.swift
@@ -4,13 +4,14 @@ import AppKit
 
 struct SidebarView: View {
     @Binding var selection: SidebarSelection
+    @ObservedObject private var languageManager = LanguageManager.shared
     var body: some View {
         VStack(spacing: 8) {
             VStack {
                 Image(nsImage: NSApp.applicationIconImage)
                     .resizable()
                     .frame(width: 48, height: 48)
-                Text("Desktop Video")
+                Text(L("Desktop Video"))
                     .font(.footnote)
             }
             .frame(maxWidth: .infinity)
@@ -19,11 +20,11 @@ struct SidebarView: View {
             ForEach(SidebarSelection.allCases, id: \.self) { item in
                 switch item {
                 case .wallpaper:
-                    SidebarItem(icon: "sparkles", name: "Wallpaper", selection: item, current: $selection)
+                    SidebarItem(icon: "sparkles", name: LocalizedStringKey(L("Wallpaper")), selection: item, current: $selection)
                 case .playback:
-                    SidebarItem(icon: "bolt.circle", name: "Playback", selection: item, current: $selection)
+                    SidebarItem(icon: "bolt.circle", name: LocalizedStringKey(L("Playback")), selection: item, current: $selection)
                 case .general:
-                    SidebarItem(icon: "gearshape", name: "General", selection: item, current: $selection)
+                    SidebarItem(icon: "gearshape", name: LocalizedStringKey(L("General")), selection: item, current: $selection)
                 }
             }
             Spacer()
